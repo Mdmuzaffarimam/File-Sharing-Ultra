94fc46111d92ca5652731edc850d92707ec30487
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2025-12-20 12:16:36 +0530

    Update FORCE_CHANNEL2 default value in config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "samanthasaintf0")
 
 # Force sub channel id, if you want enable force sub
 FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1003187326713"))
-FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1002293735035"))
+FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1002601324605"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
